9cdccc6f41552a3c10b1e8c16768bcc9fb595624
Derick Bailey <derickbailey@gmail.com>
2015-05-06 18:20:50 -0500

    removing console.logs

--- sequence/consumer/index.js
+++ sequence/consumer/index.js
@@ -25,22 +25,13 @@ Consumer.prototype.middleware = function(message, properties, actions){
   storage.getSequence(keyName, value, function(err, sequence){
     if (err) { return actions.error(err); }
 
-    console.log("msg number:", msgSeq.number);
-    console.log("last sent:", sequence.lastSent);
-    console.log("last processed:", sequence.lastProcessed);
-
     var isInOrder = (msgSeq.number === (sequence.lastProcessed + 1));
-
     if (!isInOrder){
-      console.log("----------------- OUT OF ORDER. NOT PROCESSING", msgSeq.number);
-      actions.nack();
-      return;
+      return actions.nack();
     }
 
     storage.incrementProcessed(sequence, function(err, sequence){
       if (err) { return actions.error(err); }
-
-      console.log("----------------- IN ORDER. PROCESSING", msgSeq.number);
       actions.next();
     });
   });
